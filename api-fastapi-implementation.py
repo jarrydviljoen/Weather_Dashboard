1550220ef6498e7af7176be5ad83a9c2969e00ef
Luqmaan04 <163397084+Luqmaan04@users.noreply.github.com>
2024-10-15 10:20:40 +0400

    Update api-fastapi-implementation.py

--- api-fastapi-implementation.py
+++ api-fastapi-implementation.py
@@ -2,3 +2,13 @@ from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
+@app.get("/weather")
+async def get_weather(location: str):
+    if not location:
+        raise HTTPException(status_code=400, detail="City name is required.")
+
+    # API key
+    api_key = "58f1b0ec1bbc006a4531bbe696f59cca"  
+
+    # API request URL
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric"
