f9b4649c91b0b887a1e149acb40f4124eb9e4ef0
Luqmaan04 <163397084+Luqmaan04@users.noreply.github.com>
2024-10-15 10:20:59 +0400

    Update api-fastapi-implementation.py

--- api-fastapi-implementation.py
+++ api-fastapi-implementation.py
@@ -12,3 +12,11 @@ async def get_weather(location: str):
 
     # API request URL
     url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric"
+    # Send the request to API
+    response = requests.get(url)
+
+    # See if the request was successful
+    if response.status_code == 200:
+        return response.json()  # Return the weather data as JSON
+    else:
+        raise HTTPException(status_code=404, detail="City not found.")
